8715a8f0ceb2faf8ae98173db87eccb2c1788d81
Junior <edcaiaffa@gmail.com>
2026-06-19 18:11:55 -0300

    Add files via upload

--- cria_func.c
+++ cria_func.c
@@ -1,64 +1,106 @@
-/* Pedro Henrique Padilha Valente 2510382 */
-/* Edmilson Caiaffa Junior 1911784 */
+/* Edmilson Caiaffa Junior 1911784 3WA */
+/* Pedro Henrique Padilha Valente 2510382 3WA */
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "cria_func.h"
 
-void cria_func(void* f, DescParam params[], int n, unsigned char codigo[]) {
-    int idx = 0;
+#define cod(b) (codigo[index++] = (unsigned char)(b))
 
-    // Prólogo: alinhando a pilha
-    codigo[idx++] = 0x55;             // push %rbp
-    codigo[idx++] = 0x48; 
-    codigo[idx++] = 0x89; 
-    codigo[idx++] = 0xe5;             // mov %rsp, %rbp
+static int copia_dados(unsigned char codigo[], int index, void *origem, int tamanho) {
+    unsigned char *ptr = (unsigned char *) origem;
+    for (int i = 0; i < tamanho; i++) {
+        cod(ptr[i]);
+    }
+    return index;
+}
 
-    // Salvar os parâmetros recebidos (RDI, RSI, RDX) na pilha para não perder o valor original 
-    // caso os registradores precisem ser sobrescritos
-    codigo[idx++] = 0x57;             // push %rdi
-    codigo[idx++] = 0x56;             // push %rsi
-    codigo[idx++] = 0x52;             // push %rdx
+void cria_func (void* f, DescParam params[], int n, unsigned char codigo[]) {
+    int index = 0;
+    int registrador_index = 0;
+    // prólogo
+    cod(0x55);                                              // pushq %rbp
+    cod(0x48); cod(0x89); cod(0xe5);                        // movq %rsp, %rbp
+    cod(0x48); cod(0x83); cod(0xec); cod(0x20);             // subq $32, %rsp
+    cod(0x48); cod(0x89); cod(0x7d); cod(0xf8);             // movq %rdi, -8(%rbp)
+    cod(0x48); cod(0x89); cod(0x75); cod(0xf0);             // movq %rsi, -16(%rbp)
+    cod(0x48); cod(0x89); cod(0x55); cod(0xe8);             // movq %rdx, -24(%rbp)
 
-    int param_recebido_idx = 0;
-    
-    // Configurar os registradores para a chamada da função f (RDI, RSI, RDX)
+    // parametros
     for (int i = 0; i < n; i++) {
-        // Exemplo simplificado da lógica de preenchimento de registradores:
-        // A implementação real dos opcodes varia de acordo com o registrador alvo (RDI=1º, RSI=2º, RDX=3º)
-        // e com o tipo (INT_PAR 32-bits vs PTR_PAR 64-bits).
-        
-        if (params[i].orig_val == PARAM) {
-            // O parâmetro deve ser recebido pela nova função e repassado à função original
-            // Recupera-se o valor salvo na pilha correspondente à ordem (param_recebido_idx)
-            // e move-se para o registrador correto (RDI, RSI ou RDX).
-            param_recebido_idx++;
-        } 
-        else if (params[i].orig_val == FIX) {
-            // O parâmetro deve ser amarrado a um valor constante
-            // Deve-se injetar o opcode de 'mov' com o valor imediato extraído de params[i].valor
-        } 
+        if (params[i].orig_val == FIX) {
+            if (params[i].tipo_val == INT_PAR) {
+                if (i == 0){ cod(0xbf); }                   // movl $valor, %edi
+    
+                else if (i == 1){ cod(0xbe); }              // movl $valor, %esi
+                
+                else if (i == 2){ cod(0xba); }              // movl $valor, %edx
+                
+                // copia os bytes do inteiro
+                index = copia_dados(codigo, index, &params[i].valor.v_int, 4);               
+            }
+            else if (params[i].tipo_val == PTR_PAR) {
+                if (i == 0){ cod(0x48); cod(0xbf); }        // movq $ponteiro, %rdi
+                else if (i == 1){ cod(0x48); cod(0xbe); }   // movq $ponteiro, %rsi
+                else if (i == 2){ cod(0x48); cod(0xba); }   // movq $ponteiro, %rdx
+
+                // copia os bytes do ponteiro
+                index = copia_dados(codigo, index, &params[i].valor.v_ptr, 8);
+            }
+        }
+        else if (params[i].orig_val == PARAM) {
+            unsigned char lugar_pilha;
+
+            if (registrador_index == 0) {
+                lugar_pilha = 0xf8;                         // -8(%rbp)
+            }
+            else if (registrador_index == 1) {
+                lugar_pilha = 0xf0;                         // -16(%rbp)
+            }
+            else if (registrador_index == 2) {
+                lugar_pilha = 0xe8;                         // -24(%rbp)
+            }
+
+            if (params[i].tipo_val == PTR_PAR) {
+                cod(0x48);                                  // se for ponteiro vira %rdi/%rsi/%rdx
+            }
+
+            if (i == 0) {
+                cod(0x8b); cod(0x7d); cod(lugar_pilha);     // mov -lugar_pilha(%rbp), %edi/%rdi
+            }
+            else if (i == 1) {
+                cod(0x8b); cod(0x75); cod(lugar_pilha);     // mov -lugar_pilha(%rbp), %esi/%rsi
+            }
+            else if (i == 2) {
+                cod(0x8b); cod(0x55); cod(lugar_pilha);     // mov -lugar_pilha(%rbp), %edx/%rdx
+            }
+
+            registrador_index++;
+        }
         else if (params[i].orig_val == IND) {
-            // O parâmetro deve ser amarrado a uma variável através de seu endereço
-            // Movemos params[i].valor.v_ptr para %rax, e depois desreferenciamos movendo (%rax) para o registrador alvo
+            cod(0x48); cod(0xb8);                           // movq $ponteiro, %rax
+
+            // copia os bytes do ponteiro
+            index = copia_dados(codigo, index, &params[i].valor.v_ptr, 8);
+            
+            if (params[i].tipo_val == PTR_PAR) {
+                cod(0x48);                                  // se for ponteiro vira %rdi/%rsi/%rdx
+            }
+            if (i == 0) {
+                cod(0x8b); cod(0x38);                       // mov (%rax), %edi/%rdi
+            }
+            else if (i == 1) {
+                cod(0x8b); cod(0x30);                       // mov (%rax), %esi/%rsi
+            }
+            else if (i == 2) {
+                cod(0x8b); cod(0x10);                       // mov (%rax), %edx/%rdx
+            }
         }
     }
 
-    // Preparar o endereço da função original e realizar a chamada indireta
-    // mov f, %rax
-    codigo[idx++] = 0x48; 
-    codigo[idx++] = 0xb8;
-    
-    unsigned long long func_addr = (unsigned long long) f;
-    memcpy(&codigo[idx], &func_addr, sizeof(unsigned long long));
-    idx += sizeof(unsigned long long);
-
-    // call *%rax
-    codigo[idx++] = 0xff; 
-    codigo[idx++] = 0xd0;
-
-    // Epílogo: desfazer o registro de ativação e retornar
-    codigo[idx++] = 0xc9;             // leave
-    codigo[idx++] = 0xc3;             // ret
+    // final
+    cod(0x48); cod(0xb8);                                   // movq $ponteiro, %rax
+    index = copia_dados(codigo, index, &f, 8);
+    cod(0xff); cod(0xd0);                                   // call *%rax
+    cod(0xc9);                                              // leave
+    cod(0xc3);                                              // ret
 }
